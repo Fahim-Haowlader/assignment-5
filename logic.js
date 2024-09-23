55f29f1ef84ccaa07c5a6ff51a52983aa40694ae
Fahim-Haowlader <fahimhaowlader04@gmail.com>
2024-09-23 21:35:53 +0600

    havving problem

---
+++ logic.js
@@ -0,0 +1,16 @@
+// function pageView (id1,id2 ){
+//     console.log(id1)
+    
+//     document.getElementById(id1).addEventListener('click',()=>{
+//         console.log(document.getElementById(id1))
+//         document.getElementById(id2).style.display = 'none';
+//         document.getElementById(id1).style.display = 'block';
+//     })  
+
+//     document.getElementById(id2).addEventListener('click',()=>{
+//         console.log(document.getElementById(id2))
+//         document.getElementById(id1).style.display = 'none';
+//         document.getElementById(id2).style.display = 'block';
+//     })                                   
+
+// }
---
+++ main.js
@@ -0,0 +1,27 @@
+ let blogButton = document.getElementById('blog-button')
+
+blogButton.addEventListener("click",()=>{
+    window.location.href = "./faq.html"
+})
+
+
+ let donationHistory = document.getElementById('donation-history')
+ console.log(donationHistory)
+ donationHistory.addEventListener('click',()=>{
+console.log("hi")
+ })
+
+
+
+// pageView("campains","donation-history")
+
+// document.getElementById("campains").addEventListener('click',()=>{
+//     console.log(document.getElementById("campains"))
+// //     document.getElementById('donation-history').style.display = 'none';
+// //     document.getElementById("campains").style.display = 'block';
+// })  
+// document.getElementById('donation-history').addEventListener('click',()=>{
+//     console.log(document.getElementById('donation-history'))
+// //     document.getElementById("campains").style.display = 'none';
+// //     document.getElementById('donation-history').style.display = 'block';
+// })  
